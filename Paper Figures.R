7f81fdf02d445d6a170b55c7c819a8bf1122c520
NDeeSeee <70805946+NDeeSeee@users.noreply.github.com>
2023-05-09 22:28:49 -0400

    Added Fig XC

--- Paper Figures.R
+++ Paper Figures.R
@@ -123,3 +123,72 @@ var(KRAS_only_AURKA_CNA_log2)
 
 ks.test(EGFR_only_AURKA_CNA_log2, KRAS_only_AURKA_CNA_log2)
 t.test(EGFR_only_AURKA_CNA_log2, KRAS_only_AURKA_CNA_log2)
+
+
+# Figure XC --------------------------------------------------------------------
+processed_rna_data_tp53 <- merged_data %>%
+  filter(EGFR != KRAS) %>% 
+  select(EGFR, TP53, KRAS, AURKA_rna_exp) %>%
+  mutate(
+    EGFR_only = ifelse(EGFR == "ALT" & KRAS == "WT", "ALT", "WT"),
+    KRAS_only = ifelse(KRAS == "ALT" & EGFR == "WT", "ALT", "WT"),
+    across(
+      !AURKA_rna_exp,
+      .fns = function(x) {
+        as.factor(x)
+      }
+    )
+  ) 
+
+processed_rna_data_tp53 %>%
+  pivot_longer(cols = contains("only"),
+               names_to = "subgroup",
+               values_to = "alteration") %>%
+  filter(!alteration == "WT") %>%
+  ggplot(aes(
+    y = AURKA_rna_exp,
+    fill = subgroup,
+    x = subgroup,
+    col = TP53
+  )) +
+  geom_violin(show.legend = F) +
+  geom_boxplot(width = .2, position = position_dodge(.9)) +
+  scale_color_manual(values = c("ALT" = "gray80", "WT" = "black")) +
+  theme_minimal()
+
+ggsave("Paper Figures/Fig XC.png", dpi = 400, height = 5, width = 5, units = "in")
+
+# Statistics
+EGFR_only_TP53_mut_AURKA_rna_exp <-
+  filter(processed_rna_data_tp53, EGFR == "ALT", TP53 == "ALT")$AURKA_rna_exp
+EGFR_only_TP53_wt_AURKA_rna_exp <-
+  filter(processed_rna_data_tp53, EGFR == "ALT", TP53 == "WT")$AURKA_rna_exp
+
+KRAS_only_TP53_mut_AURKA_rna_exp <-
+  filter(processed_rna_data_tp53, KRAS == "ALT", TP53 == "ALT")$AURKA_rna_exp
+KRAS_only_TP53_wt_AURKA_rna_exp <-
+  filter(processed_rna_data_tp53, KRAS == "ALT", TP53 == "WT")$AURKA_rna_exp
+
+
+# Check for normality
+ad.test(EGFR_only_TP53_mut_AURKA_rna_exp)
+ad.test(EGFR_only_TP53_wt_AURKA_rna_exp)
+ad.test(KRAS_only_TP53_mut_AURKA_rna_exp)
+ad.test(KRAS_only_TP53_wt_AURKA_rna_exp)
+
+# Compute variance for t.test
+var(EGFR_only_TP53_mut_AURKA_rna_exp)
+# 0.88
+var(EGFR_only_TP53_wt_AURKA_rna_exp)
+# 0.71
+var(KRAS_only_TP53_mut_AURKA_rna_exp)
+# 0.73
+var(KRAS_only_TP53_wt_AURKA_rna_exp)
+# 1.03
+
+ks.test(EGFR_only_TP53_mut_AURKA_rna_exp, EGFR_only_TP53_wt_AURKA_rna_exp)
+ks.test(EGFR_only_TP53_wt_AURKA_rna_exp, KRAS_only_TP53_wt_AURKA_rna_exp)
+ks.test(KRAS_only_TP53_mut_AURKA_rna_exp, KRAS_only_TP53_wt_AURKA_rna_exp)
+ks.test(EGFR_only_TP53_mut_AURKA_rna_exp, KRAS_only_TP53_mut_AURKA_rna_exp)
+
+t.test(EGFR_only_AURKA_rna_exp, KRAS_only_AURKA_rna_exp)
