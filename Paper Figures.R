9a60ff051509962ef172893d19ae3c9cd61966a3
NDeeSeee <70805946+NDeeSeee@users.noreply.github.com>
2023-05-26 21:04:22 -0400

    Added Fig XA EGFR edition

--- Paper Figures.R
+++ Paper Figures.R
@@ -340,3 +340,57 @@ ggsurvplot(
   ggtheme = theme_light() # customize plot and risk table with a theme.
   # in legend of risk table
 )
+
+
+# Figure XA EGFR version -------------------------------------------------------
+processed_rna_data <- merged_data %>%
+  filter(EGFR != KRAS) %>%
+  select(EGFR_rna_exp, EGFR, KRAS) %>%
+  pivot_longer(
+    cols = c("EGFR", "KRAS"),
+    names_to = "gene",
+    values_to = "impact"
+  ) %>%
+  filter(impact != "WT") %>%
+  select(-impact) %>%
+  mutate(gene = as.factor(gene))
+
+processed_rna_data %>%
+  ggviolin(
+    y = "EGFR_rna_exp",
+    x = "gene",
+    fill = "gene",
+    add = "boxplot",
+    add.params = list(fill = "white")
+  ) +
+  theme(legend.position = "none") +
+  stat_compare_means(method = "wilcox.test",
+                     label.y = 4.5,
+                     label.x = 1.3)
+
+ggsave(
+  "Paper Figures/Fig XA EGFR.png",
+  dpi = 400,
+  height = 5,
+  width = 5,
+  units = "in"
+)
+
+# Statistics
+EGFR_only_EGFR_rna_exp <-
+  filter(processed_rna_data, gene == "EGFR")$EGFR_rna_exp
+KRAS_only_EGFR_rna_exp <-
+  filter(processed_rna_data, gene == "KRAS")$EGFR_rna_exp
+
+# Check for normality
+ad.test(EGFR_only_EGFR_rna_exp)
+ad.test(KRAS_only_EGFR_rna_exp)
+
+# Compute variance for t.test
+var(EGFR_only_EGFR_rna_exp)
+# 0.9
+var(KRAS_only_EGFR_rna_exp)
+# 1
+
+ks.test(EGFR_only_EGFR_rna_exp, KRAS_only_EGFR_rna_exp)
+t.test(EGFR_only_EGFR_rna_exp, KRAS_only_EGFR_rna_exp)
